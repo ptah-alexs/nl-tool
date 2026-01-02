d694b671127037847a861c425b8a5bb5f3c1cdf0
true-home <true-home@home-dev>
2026-01-02 18:57:01 +0400

    переписал функцию обработки списков nodes.txt  других станций

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ import (
 	"os"
 	"strings"
 	"time"
+	"slices"
 
 	ii "github.com/ptah-alexs/ii-nl-lib"
 )
+
+func rmax(arg1, arg2 int64) int64 {
+	if arg1 > arg2 {
+		return arg1
+	} else {
+		return arg2
+	}
+}
 
 func main() {
 	expf := "nodes.txt"
@@ -39,10 +48,79 @@ Options:
 `, os.Args[0])
 		os.Exit(1)
 	}
+	cmds := []string{"check", "export", "import", "info", "sync", "check2"}
+	if !slices.Contains(cmds, args[0]) {
+		fmt.Printf("Wrong cmd: %s\n", args[0])
+		os.Exit(1)
+	}
+
+	nl := ii.OpenNL(*db_opt)
+	now := time.Now().Unix()
+	path := ""
+	inactivetime := 604800
+
 	switch cmd := args[0]; cmd {
+		case "sync":
+			nls := make(map[string]ii.NodeT, 10) // общий список инстансов
+			ks := []string{} // список url  из nodes.txt
+			for _, val := range(nl.Nodes) { // обходит список станций из своего nodes.txt
+				fmt.Printf("Проверяем %s\n", val.Url)
+				// ks = append(ks, val.Url)   //  добавление url станций в общий список, нужно для сохранения последовательности при записи
+				tmp, ok := nls[val.Url] // проверяем наличие инстанса в общем списке
+				if ok { // если ключ есть, то
+					rtime := tmp.LastEx // сохраняем время последней удачной проверки инстанса
+					tmp.LastEx = rmax(rtime, val.LastEx) // присваиваем наибольшее из времен удачной проверки инстанса (из вывода с дугих станций или из нашей базы)
+					if now - tmp.LastEx < int64(inactivetime) || *ignore_opt { // если время последней удачной проверки инстанса меньше недели или установлен ключ для игнорирования времени на других станциях
+						tmp.AltPath = true // устанавливаем признак наличия инстанса на других станциях
+						tmp.Masked = false // включаем инстанс
+					} else { // если время последней удачной проверки инстанса больше недели
+						tmp.AltPath = false // снимаем признак наличия инстанса на других станциях
+						tmp.Masked = true // выключаем инстанс
+					}
+				} else { // если ключа нет, то
+					ks = append(ks, val.Url)   //  добавление url станций в общий список, нужно для сохранения последовательности при записи
+					val.AltPath = false // снимаем признак наличия инстанса на других станциях
+					tmp.Masked = false // включаем инстанс
+					tmp = val //копируем информацию об инстансе
+				}
+				nls[val.Url] = tmp // заполняем словарь
+				if val.Exclude && !*acheck_opt {continue} // если станция в исключении и не установлена опция -a, пропускаем цикл
+				code, answ := ii.Getre(fmt.Sprintf("%s/nodes.txt", strings.TrimSuffix(val.Url, "/")), 2000) // пытаемся скачать nodes.txt с очередной станции
+				if code != 200 {continue} // если ошибка пропускаем цикл
+				if !strings.Contains(answ[0], "\t") {continue} // если в первой строке нет разделителя полей tab, значит формат не правильный, пропускаем цикл
+				for _, elem := range(answ) { // обходим список инстансов в nodes.txt станции
+					ns := ii.Parse(elem) // заполняем структуру информации о ноде
+					fmt.Printf("Проверяем подсписок %s\n", ns.Url)
+					tmp, ok := nls[ns.Url] // проверяем что этот инстанс у нас уже есть
+					if ok { // если ключ есть, то
+						rtime := tmp.LastEx // сохраняем время последней удачной проверки инстанса
+						tmp.LastEx = rmax(rtime, ns.LastEx) // присваиваем наибольшее из времен удачной проверки инстанса (из вывода с дугих станций или из нашей базы)
+						if now - tmp.LastEx < int64(inactivetime) || *ignore_opt { // если время последней удачной проверки инстанса меньше недели или установлен ключ для игнорирования времени на других станциях
+							tmp.AltPath = true // устанавливаем признак наличия инстанса на других станциях
+							tmp.Masked = false // включаем инстанс
+						} else { // если время последней удачной проверки инстанса больше недели
+							tmp.AltPath = false // снимаем признак наличия инстанса на других станциях
+							tmp.Masked = true // выключаем инстанс
+						}
+					} else { // если ключа нет, то
+						ks = append(ks, ns.Url)   //  добавление url станций в общий список, нужно для сохранения последовательности при записи
+						ns.AltPath = false // снимаем признак наличия инстанса на других станциях
+						tmp.Masked = false // включаем инстанс
+						tmp = ns //копируем информацию об инстансе
+					}
+					nls[ns.Url] = tmp // заполняем словарь
+				}
+			}
+			nl.Nodes = []ii.NodeT {}
+			for _, val := range(ks) { // проходим по списку адресов станций нашей базы
+				if *skip_opt { // если включена опция -s,
+					nl.Nodes = append(nl.Nodes, nls[val]) // то пропускаем проверку на ii-шность, и добавляем инстанс из общего списка в базу
+				} else if ii.CheckII(nls[val].Url){ // в противном случае, проверяем что инстанс является станцией ii, тогда
+					nl.Nodes = append(nl.Nodes, nls[val]) // добавляем инстанс из общего списка в базу
+				}
+			}
+			nl.Write(nl.Path) // пишем базу в файл
 	case "check":
-		nl := ii.OpenNL(*db_opt)
-		now := time.Now().Unix()
 		for idx, val := range(nl.Nodes) {
 			if val.Masked && !*acheck_opt {continue}
 			if val.Exclude {continue}
@@ -52,18 +130,21 @@ Options:
 			} else {
 				if (now - nl.Nodes[idx].LastEx) > 604800 {nl.Nodes[idx].Masked = true}
 			}
-			if val.AltPath {nl.Nodes[idx].Masked = false}
+			if val.AltPath && (now - nl.Nodes[idx].LastEx) < 604800 {
+				nl.Nodes[idx].Masked = false
+			} else {
+				nl.Nodes[idx].AltPath = false
+			}
 		}
 		nl.Write(*db_opt)
 	case "info":
-		nl := ii.OpenNL(*db_opt)
 		widthu, widthn := 0, 0
 		for _, val := range(nl.Nodes) {
 			if a:= len(val.Url); a > widthu {widthu = a}
 			if b:= len(val.Name);b > widthn {widthn = b}
 		}
-		fmt.Printf("|    | Ссылка%s | Имя%s | Дата успешной проверки | Выкл  | Alt | Искл  |\n", strings.Repeat(" ", widthu - 6), strings.Repeat(" ", widthn - 3))
-		fmt.Printf("|----|--------%s|-----%s|------------------------|-------|-----|-------|\n", strings.Repeat("-", widthu - 6), strings.Repeat("-", widthn - 3))
+		fmt.Printf("|     | Ссылка%s | Имя%s | Дата успешной проверки | Выкл  | Alt | Искл  |\n", strings.Repeat(" ", widthu - 6), strings.Repeat(" ", widthn - 3))
+		fmt.Printf("|-----|--------%s|-----%s|------------------------|-------|-----|-------|\n", strings.Repeat("-", widthu - 6), strings.Repeat("-", widthn - 3))
 		for indx, val := range(nl.Nodes) {
 			rpu := widthu - len(val.Url)
 			rpn := widthn - len(val.Name)
@@ -71,70 +152,10 @@ Options:
 			if val.Masked {bm = "+"}
 			if val.AltPath {ba = "+"}
 			if val.Exclude {be = "+"}
-			fmt.Printf("| % 2d | %s%s | %s%s | %s    |   %s   |  %s  |   %s   |\n", indx+1, val.Url, strings.Repeat(" ", rpu), val.Name, strings.Repeat(" ", rpn), time.Unix(val.LastEx, 0).Format("2006-01-02 15:04:05"), bm, ba, be)
+			fmt.Printf("| % 3d | %s%s | %s%s | %s    |   %s   |  %s  |   %s   |\n", indx+1, val.Url, strings.Repeat(" ", rpu), val.Name, strings.Repeat(" ", rpn), time.Unix(val.LastEx, 0).Format("2006-01-02 15:04:05"), bm, ba, be)
 		}
-	case "sync":
-		nl := ii.OpenNL(*db_opt)
-		nls := make(map[string]ii.NodeT, 10)
-		ks := []string{} // список url  из nodes.txt
-		change := false // признак изменений в списке
-		now := time.Now().Unix()
-		var gtime int64 = 0
-		for _, val := range(nl.Nodes) { // обходит список станций из nodes.txt
-			ks = append(ks, val.Url)   //  добавление url станции в общий список
-			if val.Exclude && !*acheck_opt {continue} // если станция в исключении и не установлена опция -a, пропускаем цикл
-			code, answ := ii.Getre(fmt.Sprintf("%s/nodes.txt", strings.TrimSuffix(val.Url, "/")), 2000) // пытаемся скачать nodes.txt с очередной станции
-			if code != 200 {continue} // если ошибка пропускаем цикл
-			if !strings.Contains(answ[0], "\t") {continue} // если в первой строке нет разделителя полей tab, значит формат не правильный, пропускаем цикл
-			for _, elem := range(answ) { // обходим список инстансов в nodes.txt станции
-				ns := ii.Parse(elem) // заполняем структуру информации о ноде
-				tmp, ok := nls[ns.Url] // проверяем что этот инстанс у нас уже есть
-				if !ok { // если нет,
-					nls[ns.Url] = ns // добавляем в общий список
-				} else { // если есть,
-					if ns.LastEx > tmp.LastEx { // проверяем что время последней проверки из nodes.txt станции больше чем сохранённое у нас в общем списке
-						tmp.LastEx = ns.LastEx // обновляем поле структуры
-						nls[ns.Url] = tmp // записываем изменения в общий список
-					}
-				}
-			}
-		}
-		for idx, val := range(ks) { // проходим по списку адресов станций нашей базы
-			tmp, ok := nls[val] // загружаем структуру для работы
-			if ok { // проверка что по данному ключу есть данные
-				change = false // устанавливаем признак изменений в выкл
-				if tmp.LastEx > nl.Nodes[idx].LastEx { // если время последнего обновления в nodes.txt больше чем у нас в базе
-					change = true // устанавливаем признак изменений во вкл
-					gtime = tmp.LastEx // запоминаем время изменения
-				} else if nl.Nodes[idx].LastEx - tmp.LastEx < 604800 { // если у нас в базе время обновления больше чем в общем списке, но разница меньше чем 7 суток
-					change = true // устанавливаем признак изменений во вкл
-					gtime = nl.Nodes[idx].LastEx // запоминаем время изменения
-				}
-				if *ignore_opt {change = true} // если установлена опция -a, устанавливаем признак изменений во вкл
-				if now - gtime > 604800 { // если самое больше время старше чем текущее время на неделю, то
-					change = false // устанавливаем признак изменений в выкл
-					nl.Nodes[idx].AltPath = false // убираем признак наличия инстанса на других станциях
-					nl.Nodes[idx].Masked = true // выключаем инстанс
-				}
-				if change { // если признак изменений вкл
-					nl.Nodes[idx].AltPath = true // устанавливаем признак того что инстанс есть в списках других станций
-					nl.Nodes[idx].Masked = false // делаем инстанс видимым в нашей базе
-				}
-				delete(nls, val) // удаляем инстанс из общего списка, потому что он уже есть в нашей базе
-			}
-		}
-		for _, val := range(nls) { //проходим по оставшемуся списку инстансов
-			if *skip_opt { // если включена опция -s,
-				nl.Nodes = append(nl.Nodes, val) // то пропускаем проверку на ii-шность, и добавляем инстанс из общего списка в базу
-			} else if ii.CheckII(val.Url){ // в противном случае, проверяем что инстанс является станцией ii, тогда
-				nl.Nodes = append(nl.Nodes, val) // добавляем инстанс из общего списка в базу
-			}
-		}
-		nl.Write(nl.Path) // пишем базу в файл
 	case "import":
-		path := ""
 		if len(args) < 2 {path = expf} else {path = args[1]}
-		nl := ii.OpenNL(*db_opt)
 		fd, err := os.Open(path)
 		if err != nil {
 			fmt.Printf("Error read file")
@@ -163,9 +184,7 @@ Options:
 		}
 		nl.Write(nl.Path)
 	case "export":
-		path := ""
 		if len(args) < 2 {path = expf} else {path = args[1]}
-		nl := ii.OpenNL(*db_opt)
 		fd, err := os.Create(path)
 		if err != nil {
 			fmt.Printf("Error open file")
@@ -181,8 +200,5 @@ Options:
 			fmt.Printf("Error write file")
 			os.Exit(1)
 		}
-	default:
-		fmt.Printf("Wrong cmd: %s\n", cmd)
-		os.Exit(1)
 	}
 }
